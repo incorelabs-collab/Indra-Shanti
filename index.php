01127cca90cdcc0a0044e407c93925a31c044b38
Kamlesh Bokdia <kbokdia@gmail.com>
2014-11-29 01:23:20 +0530

    Under Construction added

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-	echo "<h1 align='center'>Welcome to Indra Shanti Lodge. Hi!!</h1>";
+	echo "<h1 align='center'>Welcome to Indra Shanti Lodge.</h1>";
 ?>
+<img src="img/Under_Construction.jpg" />
